501bdd036016ddf01609de82042a5ab1e9af3276
lautarosoliani <lautarosoliani@hotmail.com>
2022-09-23 10:10:17 +0200

    ButtonCollapsible

---
+++ src/Components/aHeaderBar/Components/User/ButtonCollapsible.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ButtonCollapsible () {
+  return (
+    <>
+
+      <li>
+        <a class='dropdown-toggle' href='something' id='navbarDropdown' role='button' data-toggle='dropdown'>
+          Jonathan Smith
+        </a>
+        <div class='dropdown-menu'>
+          <a class='dropdown-item' href='Log out'>Log out</a>
+
+        </div>
+      </li>
+    </>
+
+  )
+}
--- src/Components/aHeaderBar/Components/User/User.jsx
+++ src/Components/aHeaderBar/Components/User/User.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import Image from './User.png'
+import ButtonCollapsible from './ButtonCollapsible'
+import ImagePNG from './User.png'
 
 export default function User () {
   return (
-    <img src={Image} alt='User Avatar' style={{ width: '45px', height: '45px', borderRadius: '50%' }} />
+    <>
+      <img src={ImagePNG} alt='User Avatar' style={{ width: '45px', height: '45px', borderRadius: '50%' }} />
+      <ButtonCollapsible />
+    </>
   )
 }
