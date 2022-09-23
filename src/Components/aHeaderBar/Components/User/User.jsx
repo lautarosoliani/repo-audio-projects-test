e2ab219ea4456fd4c6e1d3682b8c4681e62f95ff
lautarosoliani <lautarosoliani@hotmail.com>
2022-09-23 09:22:56 +0200

    User component image borderRadius refactor

--- src/Components/aHeaderBar/Components/User/User.jsx
+++ src/Components/aHeaderBar/Components/User/User.jsx
@@ -3,6 +3,6 @@ import Image from './User.png'
 
 export default function User () {
   return (
-    <img src={Image} alt='User Avatar' />
+    <img src={Image} alt='User Avatar' style={{ width: '45px', height: '45px', borderRadius: '50%' }} />
   )
 }
